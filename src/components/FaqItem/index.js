6abdf8ecc229e31a77acb421b94af696e3724804
sdenvs <sdenvs@gmail.com>
2021-12-17 13:54:22 +0530

    change-1

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,34 @@
-// Write your code here.
+import './index.css'
+
+const FaqItem = props => {
+  const {details, toggleAnsFun} = props
+  const {questionText, answerText, id, hidden} = details
+  const toggleAns = () => {
+    toggleAnsFun(id)
+  }
+  const imgSrc = hidden
+    ? 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+
+  const imgAlt = hidden ? 'plus' : 'minus'
+  return (
+    <li className="border border-dark listItem p-3 mb-3">
+      <div className="d-flex justify-content-between">
+        <h1 className="h5">{questionText}</h1>
+        <button onClick={toggleAns} className="bg-transparent border-0">
+          <img className="image" src={imgSrc} alt={imgAlt} />
+        </button>
+      </div>
+      {hidden ? (
+        ''
+      ) : (
+        <div>
+          <hr />
+          <p>{answerText}</p>
+        </div>
+      )}
+    </li>
+  )
+}
+
+export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,43 @@
-// Write your code here.
+import {Component} from 'react'
+import FaqItem from '../FaqItem'
+import './index.css'
+
+class Faqs extends Component {
+  constructor(props) {
+    super(props)
+    const {faqsList} = this.props
+    const newList = faqsList.map(eachItem => ({...eachItem, hidden: true}))
+    this.state = {faqsList: newList}
+  }
+
+  toggleAnsFun = id => {
+    this.setState(prev => ({
+      faqsList: prev.faqsList.map(eachItem => {
+        if (eachItem.id === id) {
+          return {...eachItem, hidden: !eachItem.hidden}
+        }
+        return eachItem
+      }),
+    }))
+  }
+
+  render() {
+    const {faqsList} = this.state
+    return (
+      <div className="d-flex flex-column align-items-center">
+        <h1 className="heading">FAQs</h1>
+        <ul className="ulist p-3">
+          {faqsList.map(eachItem => (
+            <FaqItem
+              toggleAnsFun={this.toggleAnsFun}
+              key={eachItem.id}
+              details={eachItem}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Faqs
